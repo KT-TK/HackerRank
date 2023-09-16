22c85d180f31b56c52d2928d07ce89db15c18758
Kshitij Tyagi <84868765+KT-TK@users.noreply.github.com>
2023-09-16 17:45:11 +0530

    Update Java Date and Time.java

--- JAVA/Java Date and Time.java
+++ JAVA/Java Date and Time.java
@@ -1,17 +1,6 @@
 
 
 class Result {
-
-    /*
-     * Complete the 'findDay' function below.
-     *
-     * The function is expected to return a STRING.
-     * The function accepts following parameters:
-     *  1. INTEGER month
-     *  2. INTEGER day
-     *  3. INTEGER year
-     */
-
     public static String findDay(int month, int day, int year) {
         Calendar cal = Calendar.getInstance();
         cal.set(Calendar.MONTH, month-1);
